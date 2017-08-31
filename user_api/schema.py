aec9d2374c85819964d08676846a04a16ce4b488
Raissa Ferreira <rai200890@gmail.com>
2017-08-31 15:23:08 -0300

    Implement a query filter connection

--- user_api/schema.py
+++ user_api/schema.py
@@ -1,10 +1,71 @@
-import graphene
+import re
 
+import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyObjectType
 
-
 from .models import User, Phone
+
+
+class FilterQueryBuilder(object):
+
+    def __init__(self, base_query, args):
+        self.base_query = base_query
+        self.args = args
+
+    @property
+    def entity(self):
+        return self.base_query.column_descriptions[0]["entity"]
+
+    def build(self):
+        query = self.base_query
+        for field_name, value in self.args.items():
+            query = query.filter(self._build_filter_predicate(field_name, value))
+        return query
+
+    def _build_filter_predicate(self, name, value):
+        field_name, insensitive_case, matcher = re.match(r"(.*)_(I?)(.*)", name).groups()
+        field = getattr(self.entity, field_name.lower())
+        operator = "like"
+        if insensitive_case:
+            operator = "ilike"
+        matchers = {
+            "startswith": "{}%",
+            "endswith": "%{}",
+            "contains": "%{}%",
+            "exact": "{}"
+        }
+        template = matchers[matcher.lower()]
+        return getattr(field, operator)(template.format(value))
+
+
+class QueryResolver(object):
+
+    def __init__(self, info, **args):
+        self.info = info
+        self.args = args
+
+    @property
+    def base_query(self):
+        graphene_type = self.info.return_type.graphene_type
+        if issubclass(graphene_type, relay.Connection):
+            graphene_type = graphene_type.Edge.node.type
+        base_query = graphene_type.get_query(self.info)
+        return base_query
+
+    @property
+    def filter_args(self):
+        return {key: value for key, value in self.args.items()
+                if key not in ["first", "before", "after", "last"]}
+
+    def resolve(self):
+        query = FilterQueryBuilder(self.base_query, self.filter_args).build()
+        return query
+
+
+def resolve_query(root, info, **args):
+    query = QueryResolver(info, **args).resolve()
+    return query.all()
 
 
 class NodeInterface(graphene.Interface):
@@ -32,21 +93,18 @@ class PhoneType(SQLAlchemyObjectType, NodeInterface):
         interfaces = [relay.Node]
 
 
+class SQLAlchemyFilterConnectionField(relay.ConnectionField):
+
+    def __init__(self, connection, **fields):
+        return super(SQLAlchemyFilterConnectionField, self).__init__(connection,
+                                                                     resolver=resolve_query,
+                                                                     **fields)
+
+
 class Query(graphene.ObjectType):
     user = relay.Node.Field(UserType)
-    users = relay.ConnectionField(UserConnection,
-                                  name=graphene.String(name="name_Istartswith"))
-
-    def resolve_users(self, info, **args):
-        query = UserType.get_query(info)
-        filters = {key: value for key, value in args.items()
-                   if key not in ["first", "before", "after", "last"]}
-        for key, value in filters.items():
-            field = getattr(User, key)
-            predicate = field.ilike("{}%".format(value))
-            query = query.filter(predicate)
-
-        return query.all()
+    users = SQLAlchemyFilterConnectionField(UserConnection,
+                                            name_Istartswith=graphene.String(name="name_Istartswith"))
 
 
 schema = graphene.Schema(query=Query)
